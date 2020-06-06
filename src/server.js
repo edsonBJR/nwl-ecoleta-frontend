f7eab586a48ba00bfa63aa3c9b52e2319632e59e
edsonBJR <ebjr.tux@gmail.com>
2020-06-06 18:37:40 -0300

    Configurado o backend no projeto

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+// configurar pasta public
+server.use(express.static("public"))
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// caminho página inicial
+// req: Requisição
+// res: Resposta 
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+// ligar o servidor
+server.listen(3000)
+
